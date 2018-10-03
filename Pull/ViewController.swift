ad588f48732c5eb49d1c34c30ec3571515ffa887
Matt Deuschle <mdeuschle@gmail.com>
2018-10-02 21:04:28 -0700

    addA

--- Pull/ViewController.swift
+++ Pull/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Master")
+        print("MasterA")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
