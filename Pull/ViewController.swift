5ff5ba2d35124c1401af588338fafeb01ef73539
Matt Deuschle <mdeuschle@gmail.com>
2018-10-02 21:15:36 -0700

    World

--- Pull/ViewController.swift
+++ Pull/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hey")
+        print("World")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
