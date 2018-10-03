8527d539adfd0cb609368ca78907ee63409cf9d8
Matt Deuschle <mdeuschle@gmail.com>
2018-10-02 20:59:55 -0700

    print master

---
+++ Pull/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pull
+//
+//  Created by Matt Deuschle on 10/2/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Master")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
