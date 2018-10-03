08df9b102b17b992a0312162eb4963979bf3d8a9
Matt Deuschle <mdeuschle@gmail.com>
2018-10-02 21:47:49 -0700

    new var (#3)

--- Pull/ViewController.swift
+++ Pull/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var masterVars = 1
+        var branchVar = 1
         // Do any additional setup after loading the view, typically from a nib.
     }
 
