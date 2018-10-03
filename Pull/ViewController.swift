8857904406a2c1c016ed3f8ff3cd0eb721156e23
Matt Deuschle <mdeuschle@gmail.com>
2018-10-02 21:01:00 -0700

    print branch a

--- Pull/ViewController.swift
+++ Pull/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Master")
+        print("BranchA")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
